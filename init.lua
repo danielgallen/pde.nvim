e051e7d9ccaf4701e9fb3061315d7da4d992ca44
Daniel Allen <1allendan2@gmail.com>
2024-01-28 19:48:32 +0000

    CR insert S-CR replace

--- init.lua
+++ init.lua
@@ -543,8 +543,12 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<S-CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     },
     ['<Tab>'] = cmp.mapping(function(fallback)

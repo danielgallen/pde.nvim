2b1f2c212d3b11fd5183e12a1e6bef2188a700b0
Daniel Allen <1allendan2@gmail.com>
2024-03-21 20:12:16 +0000

    Update telescope file browser

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -1,10 +1,24 @@
 -- File: lua/custom/plugins/telescope-file-browser.lua
 
 return {
-  {
-    'nvim-telescope/telescope-file-browser.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-  },
+  -- Define the telescope-file-browser plugin with its dependencies
+  'nvim-telescope/telescope-file-browser.nvim',
+  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
 
-  vim.api.nvim_set_keymap('n', '<space>fb', ':Telescope file_browser<CR>', { noremap = true }),
+  -- Set up the plugin after it's been loaded
+  config = function()
+    require('telescope').setup {
+      extensions = {
+        file_browser = {
+          hijack_netrw = true,
+        },
+      },
+    }
+
+    -- Load the file browser extension
+    require('telescope').load_extension 'file_browser'
+
+    -- Set the keymap for launching the file browser
+    vim.api.nvim_set_keymap('n', '<space>fb', ':Telescope file_browser<CR>', { noremap = true, silent = true })
+  end,
 }

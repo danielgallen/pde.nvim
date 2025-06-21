841de40226d1756a73452cd6213e7d42467b7a8f
Daniel Allen <dallen@danielallen.io>
2025-06-21 12:43:29 +0100

    Change copilot keybinding to <C-y>

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -4,6 +4,6 @@ return {
   'github/copilot.vim',
   config = function()
     vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_keymap('i', '<C-j>', 'copilot#Accept("<CR>")', { expr = true, silent = true, script = true })
+    vim.api.nvim_set_keymap('i', '<C-y>', 'copilot#Accept("<CR>")', { expr = true, silent = true, script = true })
   end,
 }
